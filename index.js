ed7dc479376967224088bdaa32497f7c21de3948
Yunuslala <saifjava2@gmail.com>
2023-02-15 17:41:57 +0530

    completed

---
+++ index.js
@@ -0,0 +1,8 @@
+// const  GitHubCalendar=require("react-github-calendar");
+
+    GitHubCalendar(".calendar", "Yunuslala");
+
+    // or enable responsive functionality:
+    GitHubCalendar(".calendar", "Yunuslala", { responsive: true });
+
+  
