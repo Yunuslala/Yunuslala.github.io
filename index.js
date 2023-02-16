e21b8f988e01fcf49678902e1db24867f11110ce
Yunuslala <saifjava2@gmail.com>
2023-02-17 02:14:08 +0530

    done

--- index.js
+++ index.js
@@ -5,4 +5,11 @@
     // or enable responsive functionality:
     GitHubCalendar(".calendar", "Yunuslala", { responsive: true });
 
-  
+    // https://drive.google.com/uc?export=download&id=1Ljz0Hmw8wYLQ_0huRCAlMipTDSTYMEBv
+    // https://drive.google.com/uc?export=download&id=_QolGNWL6GLQRNBmnJ88ZH2aM-WwbjG
+    //https://drive.google.com/file/d/1d_QolGNWL6GLQRNBmnJ88ZH2aM-WwbjG/view?usp=share_link
+
+    function downloadresume(){
+        window.open("https://export-download.canva.com/yOKTU/DAFZPIyOKTU/108/0-47140908991.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20230216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230216T155710Z&X-Amz-Expires=23509&X-Amz-Signature=ff74c63c336ba3bd7956a2f33c3bf67fc07b723bed992edf40f9487275da442c&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Mohd.-Yunus-Resume.pdf&response-expires=Thu%2C%2016%20Feb%202023%2022%3A28%3A59%20GMT")
+
+    }
