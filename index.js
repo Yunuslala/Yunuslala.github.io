01a7a8eaa586f2cc5090c95575c570150b914b3b
Yunuslala <saifjava2@gmail.com>
2023-02-17 17:42:53 +0530

    resposnive project part

--- index.js
+++ index.js
@@ -10,6 +10,6 @@
     //https://drive.google.com/file/d/1d_QolGNWL6GLQRNBmnJ88ZH2aM-WwbjG/view?usp=share_link
 
     function downloadresume(){
-        window.open("https://export-download.canva.com/yOKTU/DAFZPIyOKTU/108/0-47140908991.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20230216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230216T155710Z&X-Amz-Expires=23509&X-Amz-Signature=ff74c63c336ba3bd7956a2f33c3bf67fc07b723bed992edf40f9487275da442c&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Mohd.-Yunus-Resume.pdf&response-expires=Thu%2C%2016%20Feb%202023%2022%3A28%3A59%20GMT")
+        window.open("https://export-download.canva.com/yOKTU/DAFZPIyOKTU/110/0-3852512693.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJHKNGJLC2J7OGJ6Q%2F20230216%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230216T191618Z&X-Amz-Expires=67054&X-Amz-Signature=1f6a9c21edb5a2af6ef3d4b064a43916a4dd3c82f4483adf55fcf4d686e763f2&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%2A%3DUTF-8%27%27Mohd.-Yunus-Resume.pdf&response-expires=Fri%2C%2017%20Feb%202023%2013%3A53%3A52%20GMT")
 
     }
