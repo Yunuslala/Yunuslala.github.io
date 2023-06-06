e5aa49688f82a573d10ee7e33cb219ca0d4f9869
Yunuslala <saifjava2@gmail.com>
2023-06-06 17:29:47 +0530

    update resume and profile pic

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ toggle.addEventListener("click", ()=> {
     // GitHubCalendar(".calendar", "Yunuslala", { responsive: true });
 
     function downloadresume(){
-        window.open("https://drive.google.com/file/d/1tzBJQyJnqIRpQY401dhZBt5XPKL6Ow91/view?usp=share_link","_blank")
+        window.open("https://drive.google.com/file/d/1kysF30lViIkK5HwPjkihHAboxffhxCjX/view?usp=sharing","_blank")
 
     }
     // document.querySelector(".mobile-menu").addEventListener("click",()=>{
     //     document.querySelector(".mobile-menu").setAttribute("class","show")
     // })
+
+    // https://drive.google.com/file/d/1kysF30lViIkK5HwPjkihHAboxffhxCjX/view?usp=sharing
